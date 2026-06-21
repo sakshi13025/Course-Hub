62b9762be7fa6b9cecdbdf43d08135a04f79d168
sakshi13025 <sakshi13025@gmail.com>
2026-06-21 09:40:18 +0530

    Backend Part Done

---
+++ Server/src/models/courseModel.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      default: "",
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    level: {
+      type: String,
+      enum: ["beginner", "intermediate", "advanced"],
+      default: "beginner",
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "category",
+      required: true,
+    },
+    instructorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("course", courseSchema);
